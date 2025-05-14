2b7e768bcba8605ee9ecb049c181d5211202aca4
miraraeri <milana10004@gmail.com>
2025-05-14 18:21:26 +0300

    upd port

--- server.py
+++ server.py
@@ -394,7 +394,8 @@ def error():
 
 def main():
     global_init('db/web_novel.db')
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
 
 if __name__ == '__main__':
